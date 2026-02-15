46a78b530a01e0460ea2e7453e4bc9cc5f28f1ac
anoopsoni1 <sonianup8965@gmail.com>
2026-02-16 00:51:15 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express() ;
 
 
 app.use(cors({
-     origin :"http://localhost:5173",
+     origin :["http://localhost:5173","https://resume-ai-frontend-bay.vercel.app"],
     credentials: true
 }))
 
